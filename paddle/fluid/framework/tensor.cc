ca37b594f470b6bcc0971ce44596676712cc0113
Minhao Jin <minhaoj@andrew.cmu.edu>
2021-07-06 18:50:50 +0800

    update

--- paddle/fluid/framework/tensor.cc
+++ paddle/fluid/framework/tensor.cc
@@ -71,6 +71,8 @@ void* Tensor::mutable_data(const platform::Place& place,
     size = requested_size;
   }
   VLOG(1) << "Tensor MutableData is being invoked";
+  VLOG(1) << holder_ == nullptr << " " << !(holder_->place() == place) && !is_cuda_pinned_place(holder_->place()
+    << " " << holder_->size() < size + offset_;
   /* some versions of boost::variant don't have operator!= */
   if (holder_ == nullptr || (!(holder_->place() == place) && !is_cuda_pinned_place(holder_->place())) ||
       holder_->size() < size + offset_) {
